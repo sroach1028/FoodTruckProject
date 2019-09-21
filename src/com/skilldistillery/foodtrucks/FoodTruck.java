dd9ae1a08c316cdc76677728cba58960bf59fcdd
Shaun Roach <sroach1028@gmail.com>
2019-09-21 11:24:27 -0600

    built constructors and getters/setters in FoodTruck

--- src/com/skilldistillery/foodtrucks/FoodTruck.java
+++ src/com/skilldistillery/foodtrucks/FoodTruck.java
@@ -1,5 +1,57 @@
 package com.skilldistillery.foodtrucks;
 
 public class FoodTruck {
+	private static int TRUCK_ID = 1;
+	private String truckName;
+	private String foodType;
+	private int rating;
+	
+	public FoodTruck() {
+		
+	}
+	public FoodTruck(String truckName, String foodType, int rating) {
+		this.truckName = truckName;
+		this.foodType = foodType;
+		this.rating = rating;
+	
+	}
+	public static int getTRUCK_ID() {
+		return TRUCK_ID;
+	}
+	public static void setTRUCK_ID() {
+		TRUCK_ID ++;
+	}
+	public String getTruckName() {
+		return truckName;
+	}
+	public void setTruckName(String truckName) {
+		this.truckName = truckName;
+	}
+	public String getFoodType() {
+		return foodType;
+	}
+	public void setFoodType(String foodType) {
+		this.foodType = foodType;
+	}
+	public int getRating() {
+		return rating;
+	}
+	public void setRating(int rating) {
+		this.rating = rating;
+	}
+	
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("FoodTruck [truckName=").append(truckName).append(", foodType=").append(foodType)
+				.append(", rating=").append(rating).append("]");
+		return builder.toString();
+	}
+	
+	
+	
+	
+
+
 
 }
--- src/com/skilldistillery/foodtrucks/FoodTruckApp.java
+++ src/com/skilldistillery/foodtrucks/FoodTruckApp.java
@@ -3,7 +3,6 @@ package com.skilldistillery.foodtrucks;
 public class FoodTruckApp {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
 	}
 
