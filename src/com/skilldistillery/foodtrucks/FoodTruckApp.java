abc29aaf3f78dcef6405bf0b26c0bd1620b5c054
Shaun Roach <sroach1028@gmail.com>
2019-09-21 19:08:39 -0600

    added switch statements. broke it. then fixed it.

--- src/com/skilldistillery/foodtrucks/FoodTruckApp.java
+++ src/com/skilldistillery/foodtrucks/FoodTruckApp.java
@@ -29,11 +29,13 @@ public class FoodTruckApp {
 		do {
 			displayMenu();
 			selection = kb.nextInt();
-			if (selection == 1) {
-				if(count==trucks.length) {
+
+			switch (selection) {
+			case 1:
+				if (count == trucks.length) {
 					System.out.print("Can not hold any more trucks. Press any key to return to main menu: ");
 					kb.next();
-					continue;
+					break;
 				}
 				do {
 					System.out.print("Enter name of food truck: ");
@@ -52,36 +54,48 @@ public class FoodTruckApp {
 					if (count < trucks.length) {
 						displayMenu2();
 						selection2 = kb.nextInt();
-						if (selection2 == 1) {
+//						if (selection2 == 1) {
+//							continue;
+//						}
+//						else if(selection2==2){
+//							break;
+//						}
+						switch (selection2) {
+						case 1:
 							continue;
-						}
-						if (selection2 == 2) {
+						case 2:
 							break;
-						}
+						} // END INSIDE SWITCH
 					} else {
 						System.out.print(
 								"All full up. Can not add any more trucks. Enter any key to return to main menu: ");
 						kb.next();
 						break;
 					}
-
 				} while (selection2 == 1); // end of inside loop
-			}
-			if (selection == 2) {
-				// TODO Display all foodtrucks, in order of highest rating
+				break;
+			case 2:
 				displayTrucks();
-				System.out.print("\nPress any key to return to main menu: ");
+				System.out.print("\nEnter any key to return to main menu: ");
 				kb.next();
 				System.out.println("");
-				continue;
-				// continue to check OUTSIDE loop condition;
-
-			}
-			if (selection == 3) {
+				break;
+			case 3:
+				displayAverage();
+				System.out.print("\nEnter any key to return to main menu: ");
+				kb.next();
+				break;
+			case 4:
+				displayHighestRated();
+				System.out.print("\nEnter any key to return to main menu: ");
+				kb.next();
+				break;
+			case 5:
 				System.out.println("Thanks for using the foodtruck app, don't forget to rate us too!");
-			}
+				break;
 
-		} while (selection != 3);
+			} // END OUTSIDE SWITCH
+		} while (selection != 5);
 
 		kb.close();
 	}
@@ -98,15 +112,51 @@ public class FoodTruckApp {
 	}
 
 	private void displayMenu() {
-		System.out.print("1. Add New Food Truck\n2. Show Food Trucks\n3. Quit Food Truck App\n: ");
+		System.out.print(
+				"1. Add New Food Truck\n2. Show All Food Trucks\n3. Show Average Rating for All Trucks\n4. Show Highest Rated Truck(s)\n5. Quit Food Truck App\n--->");
 	}
 
 	private void displayMenu2() {
-		System.out.println("1. Add another truck\n2. Return to main menu\n");
+		System.out.print("1. Add another truck\n2. Return to main menu\n--->");
 	}
 
 	private void addTruck(FoodTruck truck) {
 		trucks[count] = truck;
 		count++;
 	}
+
+	private void displayAverage() {
+		int totalRatings = 0;
+		double averageRating = 0.0;
+		for (FoodTruck truck : trucks) {
+			if (truck != null) {
+				totalRatings += truck.getRating();
+			}
+		}
+		averageRating = (totalRatings) / (trucks.length);
+		System.out.println("Average rating for all food trucks is " + averageRating);
+
+	}
+
+	private void displayHighestRated() {
+		int highest = 0;
+		for (FoodTruck truck : trucks) {
+			if (truck != null) {
+				if (truck.getRating() > highest) {
+					highest = truck.getRating();
+				}
+			}
+		}
+		for (FoodTruck truck : trucks) {
+			if (truck != null) {
+				if (truck.getRating() == highest) {
+					System.out.println(truck.toString());
+				}
+			}
+		}
+	}
+	// TODO method for computing and returning an average of all ratings in the
+	// array
+	// TODO method for finding the highest rated truck and returning all its
+	// contents
 }
