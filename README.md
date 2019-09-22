eb29fd1a3ada7e84308a8a01ac5c67ae6215d82b
Shaun Roach <sroach1028@gmail.com>
2019-09-22 13:13:55 -0600

    filled README

--- README.md
+++ README.md
@@ -2,8 +2,16 @@
 
 ### Week 2 Homework Project
 
+Created by Shaun Roach, student at Skill Distillery in Denver, Colorado. 
+
 ### Overview
+This program allows a user to enter up to five "food trucks" (though it may be altered to allow more or less). The selection menu allows the user to continue adding trucks, to view an average rating of all trucks entered, to view the highest rated truck(s), or just view a list of all the trucks registered. Each truck is registered with a unique ID#, its name, its food type, and the user's rating.
+
 
 ### Technologies/Topics Used
+The user application includes one main static method, and numerous non-static, "helper methods" to display menus, collect user input, and display the information respective to the menu selection. The application makes use of a separate Java "FoodTruck" Class, which includes multiple constructors for taking the food truck information inputted by the user and creating each truck object. After being constructed, each truck object holds private attributes that can only be accessed and modified through the use of public, non-static methods contained in the FoodTruck class, called getters and setters. The class also provides a toString method, which the user application utilizes to display information on each truck.  
+
 
 ### Lessons Learned
+
+A major lesson I learned with this project is the difficulty with using multiple different Scanner methods within the same class. Using Scanner.nextInt() in conjunction with Scanner.next() and Scanner.nextLine(). It causes output to be displayed and lines to be executed in ways not intended. I mitigated this by using String data types in lieu of Integers where possible, so nextInt could be avoided.
--- src/com/skilldistillery/foodtrucks/FoodTruckApp.java
+++ src/com/skilldistillery/foodtrucks/FoodTruckApp.java
@@ -13,7 +13,8 @@ public class FoodTruckApp {
 	// MAIN METHOD
 	public static void main(String[] args) {
 		FoodTruckApp fta = new FoodTruckApp();
-		System.out.println("Welcome to the Food Truck App!\n");
+		System.out.println("\nWelcome to the Food Truck App!\n"
+				+ "-----------------------------------");
 		fta.run();
 	}
 //                     WORKER METHODS
@@ -24,40 +25,41 @@ public class FoodTruckApp {
 
 		do {
 			displayMenu();
-			selection = kb.next();
+			selection = kb.nextLine();
 
 			switch (selection) {
 			case "1":
 				if (count == trucks.length) {
-					System.out.print("Can not hold any more trucks. Press any key to return to main menu: ");
-					kb.next();
+					System.out.println("Can not hold any more trucks. Press any key to return to main menu: ");
+					kb.nextLine();
 					break;
-				} else {
+				} 
+				else {
 					newTruck(kb);
 					break;
 				}
 			case "2":
 				displayTrucks();
 				System.out.print("\nEnter any key to return to main menu: ");
-				kb.next();
+				kb.nextLine();
 				System.out.println("");
 				break;
 			case "3":
 				displayAverage();
 				System.out.print("\nEnter any key to return to main menu: ");
-				kb.next();
+				kb.nextLine();
 				break;
 			case "4":
 				displayHighestRated();
 				System.out.print("\nEnter any key to return to main menu: ");
-				kb.next();
+				kb.nextLine();
 				break;
 			case "5":
 				System.out.println("Thanks for using the foodtruck app, don't forget to rate us, too!");
 				break;
 			default:
 				System.out.print("\nDid not recognize choice. Enter any key to return to main menu: ");
-				kb.next();
+				kb.nextLine();
 			} // END OUTSIDE SWITCH
 		} while (!selection.equals("5"));
 
@@ -70,15 +72,19 @@ public class FoodTruckApp {
 		String foodType;
 		int rating;
 		do {
-			System.out.print("Enter name of food truck: ");
+			System.out.println("Enter name of food truck, or enter 'q' to return to the main menu: ");
 			name = kb.nextLine();
+			if (checkForQuit(name)){
+			break;
+			}
 
-			System.out.print("\nWhat type of food does " + name + " serve?: ");
+			System.out.println("What type of food does " + name + " serve?: ");
 			foodType = kb.nextLine();
 
-			System.out.print("\nWhat would you rate " + name + "?: ");
+			System.out.print("What would you rate " + name + "?: ");
 			rating = kb.nextInt();
-
+			kb.nextLine();
+			
 			FoodTruck newTruck = new FoodTruck(name, foodType, rating);
 			addTruck(newTruck);
 			newTruck.setTRUCK_ID();
@@ -95,7 +101,7 @@ public class FoodTruckApp {
 				} // END INSIDE SWITCH
 			} else {
 				System.out.print("All full up. Can not add any more trucks. Enter any key to return to main menu: ");
-				kb.next();
+				kb.nextLine();
 				break;
 			}
 		} while (selection2 == 1); // end of loop
@@ -105,8 +111,6 @@ public class FoodTruckApp {
 		System.out.println("");
 		for (FoodTruck truck : trucks) {
 			if (truck != null) {
-//				System.out.println("ID: " + truck.getTruckID() + "\tName: " + truck.getTruckName() + "\tFood Type: "
-//						+ truck.getFoodType() + "\t\tRating: " + truck.getRating());
 				System.out.println(truck.toString());
 			}
 
@@ -115,11 +119,11 @@ public class FoodTruckApp {
 
 	private void displayMenu() {
 		System.out.print(
-				"1. Add New Food Truck\n2. Show All Food Trucks\n3. Show Average Rating for All Trucks\n4. Show Highest Rated Truck(s)\n5. Quit Food Truck App\n--->");
+				"\n1. Add New Food Truck\n2. Show All Food Trucks\n3. Show Average Rating for All Trucks\n4. Show Highest Rated Truck(s)\n5. Quit Food Truck App\n--->");
 	}
 
 	private void displayMenu2() {
-		System.out.print("1. Add another truck\n2. Return to main menu\n--->");
+		System.out.print("\n1. Add another truck\n2. Return to main menu\n--->");
 	}
 
 	private void addTruck(FoodTruck truck) {
@@ -156,5 +160,28 @@ public class FoodTruckApp {
 				}
 			}
 		}
+	}
+	
+	boolean checkForQuit(String name) {
+		boolean result = false;
+		switch(name) {
+		case "q":
+		result = true;
+		break;
+	case "Q":
+		result = true;
+		break;
+	case "quit":
+		result = true;
+		break;
+	case "Quit":
+		result = true;
+		break;
+	case "QUIT":
+		result = true;
+		break;
+		}
+		
+		return result;
 	}
 }
