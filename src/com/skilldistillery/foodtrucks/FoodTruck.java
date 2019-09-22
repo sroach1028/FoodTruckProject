7d7dc54bf9efec90156695c74574f53275c2fcf2
Shaun Roach <sroach1028@gmail.com>
2019-09-21 22:01:36 -0600

    going to add error statements

--- src/com/skilldistillery/foodtrucks/FoodTruck.java
+++ src/com/skilldistillery/foodtrucks/FoodTruck.java
@@ -6,55 +6,62 @@ public class FoodTruck {
 	private String foodType;
 	private int rating;
 	private int truckID;
-	
+
 	public FoodTruck() {
-		
+
 	}
+
 	public FoodTruck(String truckName, String foodType, int rating) {
 		this(truckName, foodType, rating, TRUCK_ID);
-	
+
 	}
-	
+
 	public FoodTruck(String truckName, String foodType, int rating, int truckId) {
 		this.truckID = truckId;
 		this.truckName = truckName;
 		this.foodType = foodType;
 		this.rating = rating;
 	}
-	
 
 	public int getTruckID() {
 		return truckID;
 	}
+
 	public void setTruckID(int truckID) {
 		this.truckID = truckID;
 	}
+
 	public void setTRUCK_ID() {
-		TRUCK_ID ++;
-		
+		TRUCK_ID++;
+
 	}
+
 	public String getTruckName() {
 		return truckName;
 	}
+
 	public void setTruckName(String truckName) {
 		this.truckName = truckName;
 	}
+
 	public String getFoodType() {
 		return foodType;
 	}
+
 	public void setFoodType(String foodType) {
 		this.foodType = foodType;
 	}
+
 	public int getRating() {
 		return rating;
 	}
+
 	public void setRating(int rating) {
 		this.rating = rating;
 	}
-	
-	// TODO 
-	
-	
+
+	// TODO
+
 	@Override
 	public String toString() {
 		StringBuilder builder = new StringBuilder();
@@ -62,15 +69,5 @@ public class FoodTruck {
 				.append("\t Food Type: ").append(foodType).append("\t Rating: ").append(rating);
 		return builder.toString();
 	}
-	
-	
-	
-
-	
-	
-	
-	
-
-
 
 }
--- src/com/skilldistillery/foodtrucks/FoodTruckApp.java
+++ src/com/skilldistillery/foodtrucks/FoodTruckApp.java
@@ -21,54 +21,21 @@ public class FoodTruckApp {
 	private void run() {
 		Scanner kb = new Scanner(System.in);
 		String selection;
-		int selection2;
-		String name;
-		String foodType;
-		int rating;
-		// TODO OUTSIDE do-while loop, print the menu, while selection is not "quit"
+
 		do {
 			displayMenu();
 			selection = kb.next();
-			kb.nextLine();
+
 			switch (selection) {
 			case "1":
 				if (count == trucks.length) {
 					System.out.print("Can not hold any more trucks. Press any key to return to main menu: ");
 					kb.next();
 					break;
+				} else {
+					newTruck(kb);
+					break;
 				}
-				do {
-					System.out.print("Enter name of food truck: ");
-					name = kb.nextLine();
-
-					System.out.print("\nWhat type of food does " + name + " serve?: ");
-					foodType = kb.nextLine();
-
-					System.out.print("\nWhat would you rate " + name + "?: ");
-					rating = kb.nextInt();
-
-					FoodTruck newTruck = new FoodTruck(name, foodType, rating);
-					addTruck(newTruck);
-					newTruck.setTRUCK_ID();
-
-					if (count < trucks.length) {
-						displayMenu2();
-						selection2 = kb.nextInt();
-						kb.nextLine();
-						switch (selection2) {
-						case 1:
-							continue;
-						case 2:
-							break;
-						} // END INSIDE SWITCH
-					} else {
-						System.out.print(
-								"All full up. Can not add any more trucks. Enter any key to return to main menu: ");
-						kb.next();
-						break;
-					}
-				} while (selection2 == 1); // end of inside loop
-				break;
 			case "2":
 				displayTrucks();
 				System.out.print("\nEnter any key to return to main menu: ");
@@ -86,7 +53,7 @@ public class FoodTruckApp {
 				kb.next();
 				break;
 			case "5":
-				System.out.println("Thanks for using the foodtruck app, don't forget to rate us too!");
+				System.out.println("Thanks for using the foodtruck app, don't forget to rate us, too!");
 				break;
 			default:
 				System.out.print("\nDid not recognize choice. Enter any key to return to main menu: ");
@@ -97,13 +64,50 @@ public class FoodTruckApp {
 		kb.close();
 	}
 
+	private void newTruck(Scanner kb) {
+		int selection2;
+		String name;
+		String foodType;
+		int rating;
+		do {
+			System.out.print("Enter name of food truck: ");
+			name = kb.nextLine();
+
+			System.out.print("\nWhat type of food does " + name + " serve?: ");
+			foodType = kb.nextLine();
+
+			System.out.print("\nWhat would you rate " + name + "?: ");
+			rating = kb.nextInt();
+
+			FoodTruck newTruck = new FoodTruck(name, foodType, rating);
+			addTruck(newTruck);
+			newTruck.setTRUCK_ID();
+
+			if (count < trucks.length) {
+				displayMenu2();
+				selection2 = kb.nextInt();
+				kb.nextLine();
+				switch (selection2) {
+				case 1:
+					continue;
+				case 2:
+					break;
+				} // END INSIDE SWITCH
+			} else {
+				System.out.print("All full up. Can not add any more trucks. Enter any key to return to main menu: ");
+				kb.next();
+				break;
+			}
+		} while (selection2 == 1); // end of loop
+	}
+
 	private void displayTrucks() {
 		System.out.println("");
 		for (FoodTruck truck : trucks) {
 			if (truck != null) {
 //				System.out.println("ID: " + truck.getTruckID() + "\tName: " + truck.getTruckName() + "\tFood Type: "
 //						+ truck.getFoodType() + "\t\tRating: " + truck.getRating());
-			System.out.println(truck.toString());
+				System.out.println(truck.toString());
 			}
 
 		}
@@ -124,14 +128,14 @@ public class FoodTruckApp {
 	}
 
 	private void displayAverage() {
-		int totalRatings = 0;
+		double totalRatings = 0.0;
 		double averageRating = 0.0;
 		for (FoodTruck truck : trucks) {
 			if (truck != null) {
 				totalRatings += truck.getRating();
 			}
 		}
-		averageRating = (totalRatings) / (trucks.length);
+		averageRating = (totalRatings) / (count);
 		System.out.println("Average rating for all food trucks is " + averageRating);
 
 	}
@@ -153,8 +157,4 @@ public class FoodTruckApp {
 			}
 		}
 	}
-	// TODO method for computing and returning an average of all ratings in the
-	// array
-	// TODO method for finding the highest rated truck and returning all its
-	// contents
 }
